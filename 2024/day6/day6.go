2295f5b5d5901d5cb808b9bf0e36e6a6b046aec5
Mikjail Salazar <mikjailsalazar@gmail.com>
2024-12-13 19:05:31 +0100

    day6-part2: I did it! Not proud of the timing of the solution though... It takes 14s to give me the solution because I'm iterating in all possible possitions of obstacles =(

--- 2024/day6/day6.go
+++ 2024/day6/day6.go
@@ -1,6 +1,11 @@
 package day6
 
-import "adventOfCode/main/utils"
+import (
+	"adventOfCode/main/utils"
+	"fmt"
+)
+
+type GuardDirections map[string]Direction
 
 type Direction struct {
 	RowChange    int
@@ -9,9 +14,8 @@ type Direction struct {
 }
 
 func Part1() int {
-	data := utils.GetDataSplittedInLines("day6/day6")
-	path := utils.SplitStringIntoArray(data)
-	guardDirections := map[string]Direction{
+	path := getPath()
+	guardDirections := GuardDirections{
 		"^": {-1, 0, ">"},
 		">": {0, 1, "V"},
 		"V": {1, 0, "<"},
@@ -23,7 +27,8 @@ func Part1() int {
 	for i := 0; i < ROWS; i++ {
 		for j := 0; j < COLS; j++ {
 			if path[i][j] == "^" {
-				return dfs(path, guardDirections, i, j, ROWS, COLS)
+				res, _ := dfs(path, guardDirections, i, j, ROWS, COLS)
+				return res
 			}
 		}
 	}
@@ -31,26 +36,95 @@ func Part1() int {
 	return 0
 }
 
-func dfs(path [][]string, guardDirections map[string]Direction, row int, col int, ROWS int, COLS int) (result int) {
+func getPath() [][]string {
+	data := utils.GetDataSplittedInLines("day6/day6")
+	path := utils.SplitStringIntoArray(data)
+	return path
+}
+
+func dfs(path [][]string, guardDirections GuardDirections, row int, col int, ROWS int, COLS int) (result int, isLoop bool) {
 	dir := guardDirections[path[row][col]]
 	result = 1
+	isLoop = false
+	visited := make(map[string]bool) // tracking visited path
 	for {
 		nextRow := row + dir.RowChange
 		nextCol := col + dir.ColChange
+
+		key := fmt.Sprintf("%d,%d,%s", nextRow, nextCol, dir.NewDirection)
+
+		if visited[key] {
+			isLoop = true
+			return
+		}
+
+		visited[key] = true
+
 		if nextRow < 0 || nextRow >= ROWS || nextCol < 0 || nextCol >= COLS {
 			result++
 			return
 		}
+
 		if path[nextRow][nextCol] == "#" {
 			dir = guardDirections[dir.NewDirection]
 			continue
 		}
+
 		if path[row][col] == "." {
 			result++
 		}
-		path[row][col] = "X"
+
 		row = nextRow
 		col = nextCol
 	}
+}
 
+func Part2() (result int) {
+	path := getPath()
+	guardDirections := GuardDirections{
+		"^": {-1, 0, ">"},
+		">": {0, 1, "V"},
+		"V": {1, 0, "<"},
+		"<": {0, -1, "^"},
+	}
+	ROWS := len(path)
+	COLS := len(path[0])
+
+	obstaclePostitions := getObstaclePositions(path)
+
+	for _, obstacle := range obstaclePostitions {
+		for i := 0; i < ROWS; i++ {
+			for j := 0; j < COLS; j++ {
+				path[obstacle.Row][obstacle.Col] = "#"
+				if path[i][j] == "^" {
+					_, isLoop := dfs(path, guardDirections, i, j, ROWS, COLS)
+					if isLoop {
+						result++
+					}
+				}
+				path[obstacle.Row][obstacle.Col] = "."
+			}
+		}
+
+	}
+
+	return result
+}
+
+type ObstaclePositions struct {
+	Row int
+	Col int
+}
+
+func getObstaclePositions(path [][]string) (positions []ObstaclePositions) {
+	ROWS := len(path)
+	COLS := len(path[0])
+	for i := 0; i < ROWS; i++ {
+		for j := 0; j < COLS; j++ {
+			if path[i][j] == "." {
+				positions = append(positions, ObstaclePositions{i, j})
+			}
+		}
+	}
+	return positions
 }
--- 2024/day6/day6_test.go
+++ 2024/day6/day6_test.go
@@ -12,3 +12,9 @@ func TestPart1(t *testing.T) {
 
 	assert.Equal(t, data, 41)
 }
+
+func TestPart2(t *testing.T) {
+	data := day6.Part2()
+
+	assert.Equal(t, data, 1530)
+}
