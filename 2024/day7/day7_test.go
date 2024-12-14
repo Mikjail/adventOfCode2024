256852d77828d167e76a1665bdbb63e2d7f75077
Mikjail Salazar <mikjailsalazar@gmail.com>
2024-12-14 14:25:42 +0100

    day7,1: In this one I used the backtracking technique, where I check all the combinations and return the valid one (if there is one)

---
+++ 2024/day7/day7_test.go
@@ -0,0 +1,73 @@
+package day7_test
+
+import (
+	"adventOfCode/main/utils"
+	"strings"
+	"testing"
+
+	"gotest.tools/v3/assert"
+)
+
+type Equations struct {
+	Numbers []int
+	Target  int
+}
+
+func TestPart1(t *testing.T) {
+	data := Part1()
+
+	assert.Equal(t, data, 3749)
+}
+
+func Part1() int {
+	lines := utils.GetDataSplittedInLines("day7/day7")
+
+	equations := getEquations(lines)
+
+	return getCombinationResult(equations)
+
+}
+
+func getCombinationResult(equations []Equations) (result int) {
+	for _, equation := range equations {
+		if isValidBackTracking(0, 0, equation.Numbers, equation.Target) {
+			result += equation.Target
+		}
+	}
+	return
+}
+
+func isValidBackTracking(index int, current int, comb []int, target int) bool {
+	if index == len(comb) {
+		return current == target
+	}
+
+	if isValidBackTracking(index+1, current+comb[index], comb, target) {
+		return true
+	}
+
+	if isValidBackTracking(index+1, current*comb[index], comb, target) {
+		return true
+	}
+
+	return false
+}
+
+func getEquations(lines []string) (equations []Equations) {
+	for _, line := range lines {
+		data := strings.Split(line, ":")
+		result := utils.ParseStringToNum(data[0])
+		trimmedData := strings.TrimSpace(data[1])
+		numbers := getNumbers(strings.Split(trimmedData, " "))
+		equations = append(equations, Equations{Target: result, Numbers: numbers})
+	}
+	return
+}
+
+func getNumbers(numbers []string) (result []int) {
+
+	for _, number := range numbers {
+		result = append(result, utils.ParseStringToNum(number))
+	}
+	return
+}
